893200529b955410a1566e1886e9cd0ef5de4645
vuhoi292 <vuhoi290296@gmail.com>
2021-03-01 16:34:46 +0700

    Create Inline Edit

---
+++ Controller/Adminhtml/Flip/InlineEdit.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Magepow\Flipbook\Controller\Adminhtml\Flip;
+
+class InlineEdit extends \Magento\Backend\App\Action
+{
+
+    protected $jsonFactory;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\JsonFactory $jsonFactory
+    ) {
+        parent::__construct($context);
+        $this->jsonFactory = $jsonFactory;
+    }
+
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Json $resultJson */
+        $resultJson = $this->jsonFactory->create();
+        $error = false;
+        $messages = [];
+
+        if ($this->getRequest()->getParam('isAjax')) {
+            $postItems = $this->getRequest()->getParam('items', []);
+            if (!count($postItems)) {
+                $messages[] = __('Please correct the data sent.');
+                $error = true;
+            } else {
+                foreach (array_keys($postItems) as $modelid) {
+                    /** @var \Magento\Cms\Model\Block $block */
+                    $model = $this->_objectManager->create('Magepow\Flipbook\Model\Flip')->load($modelid);
+                    try {
+                        $model->setData(array_merge($model->getData(), $postItems[$modelid]));
+                        $model->save();
+                    } catch (\Exception $e) {
+                        $messages[] = "[Mytesting ID: {$modelid}]  {$e->getMessage()}";
+                        $error = true;
+                    }
+                }
+            }
+        }
+
+        return $resultJson->setData([
+            'messages' => $messages,
+            'error' => $error
+        ]);
+    }
+}
